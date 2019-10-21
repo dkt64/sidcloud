9c46ca17463f75cea67311ed2e963b1426217977
DKT <b.apanasewicz@gmail.com>
2019-10-21 20:21:19 +0200

    coś działa

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,46 @@
 <template>
   <div id="app">
     <p>Welcome to SIDCloud</p>
+
+    <!-- <div>
+    <span>Total duration: {{ duration }} seconds</span>
+    <span>Progress: {{ (progress * 100) }}%</span>
+    <button @click="togglePlayback">{{ playing ? 'Pause' : 'Play' }}</button>
+    <button @click="stop">Stop</button>
+    </div>-->
+
+    <!-- <audio id="audio" controls autoplay preload="none">
+        <source :src=query_url type="audio/wav" />
+    </audio>-->
+    <!-- <audio
+      src="http://localhost:8099/api/v1/audio"
+      id="radio"
+      preload="none"
+      type="audio/wav"
+      controls
+    ></audio>-->
+
+    <audio id="radio" controls preload="none">
+      <source src="http://localhost:8099/api/v1/audio" type="audio/wav" />
+    </audio>
+    <p />
+
+    <!-- <audio id="audio">
+        <source src="http://localhost:8099/api/v1/audio" type="audio/wav" />
+      </audio>
+      <div class="player-controls">
+        <div id="radioIcon"></div>
+        <button id="playAudio"></button>
+        <p>
+          <small id="currentTime">00:00</small>
+        </p>
+    </div>-->
+
     <input
       class="form-control"
       v-model="sid_link"
       v-on:keyup.enter="Link"
-      placeholder="Type SID file link"
+      placeholder="Type SID or PRG file link"
     />
     <p>{{ sid_data }}</p>
 
@@ -21,15 +56,20 @@
 
 <script>
 import axios from "axios";
+// import {Howl} from 'howler';
+
+// var track = new Audio("http://localhost:8099/api/v1/audio")
 
 export default {
+  // mixins: [VueHowler],
   data: function() {
     return {
       handle_id: 0,
       handle_name: null,
       info: null,
       sid_link: null,
-      sid_data: null
+      sid_data: null,
+      query_url: null
     };
   },
   name: "app",
@@ -44,9 +84,7 @@ export default {
       // eslint-disable-next-line
       console.log("Query = " + query);
 
-      axios
-        .get(query)
-        .then(response => (this.handle_name = response.data));
+      axios.get(query).then(response => (this.handle_name = response.data));
 
       // eslint-disable-next-line
       console.log("Scener " + this.handle_name);
@@ -55,16 +93,38 @@ export default {
     // Odtworzenie SIDa
     // ==========================================================
     Link: function() {
-      var query = "http://localhost:8099/api/v1/audio?sid_url=" + this.sid_link
+      var query = "http://localhost:8099/api/v1/audio?sid_url=" + this.sid_link;
       // eslint-disable-next-line
       console.log("Query = " + query);
 
-      axios
-        .post(query)
-        .then(response => (this.sid_data = response.data));
+      this.query_url = "http://localhost:8099/api/v1/audio";
+
+      // document.getElementById('audio').currentTime = 0
+      // document.getElementById('audio').play()
+
+      axios.post(query).then(response => (this.sid_data = response.data));
+      // axios.post(query);
+
+      // var sound = new Howl({
+      //   src: ["http://localhost:8099/api/v1/audio"],
+      //   format: ['wav'],
+      //   html5: true
+      // });
+
+      // sound.play()
+
+      // track.play()
+
+      // var player = document.getElementById("radio");
+      // player.pause();
+      // player.src = this.query_url;
+      // player.play()
+
+      // media.play();
+      // axios.get(this.query_url)
 
       // eslint-disable-next-line
-      console.log("SID data " + this.sid_data);
+      // console.log("SID data " + this.sid_data);
     }
   }
 };
@@ -74,7 +134,7 @@ export default {
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  /* -moz-osx-font-smoothing: grayscale; */
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
