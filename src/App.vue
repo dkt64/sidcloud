c42ae3e61127faf84952a929d93a1ea9d882695d
DKT <b.apanasewicz@gmail.com>
2019-10-21 11:40:19 +0200

    postowanie

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@
     <p>Welcome to SIDCloud</p>
     <input
       class="form-control"
+      v-model="sid_link"
+      v-on:keyup.enter="Link"
+      placeholder="Type SID file link"
+    />
+    <p>{{ sid_data }}</p>
+
+    <input
+      class="form-control"
       v-model="handle_id"
       v-on:keyup.enter="Handle"
       placeholder="Type scener handle"
@@ -19,12 +27,17 @@ export default {
     return {
       handle_id: 0,
       handle_name: null,
-      info: null
+      info: null,
+      sid_link: null,
+      sid_data: null
     };
   },
   name: "app",
   components: {},
   methods: {
+    // ==========================================================
+    // Odczyt danych o scenerze
+    // ==========================================================
     Handle: function() {
       var query =
         "https://csdb.dk/webservice/?type=scener&id=" + this.handle_id;
@@ -37,6 +50,21 @@ export default {
 
       // eslint-disable-next-line
       console.log("Scener " + this.handle_name);
+    },
+    // ==========================================================
+    // Odtworzenie SIDa
+    // ==========================================================
+    Link: function() {
+      var query = "http://localhost:8099/api/v1/audio?sid_url=" + this.sid_link
+      // eslint-disable-next-line
+      console.log("Query = " + query);
+
+      axios
+        .post(query)
+        .then(response => (this.sid_data = response.data));
+
+      // eslint-disable-next-line
+      console.log("SID data " + this.sid_data);
     }
   }
 };
