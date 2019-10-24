d96e9b710a1d9b4ec61402f3763807053999bc0e
DKT <b.apanasewicz@gmail.com>
2019-10-24 23:37:02 +0200

    vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,100 +1,36 @@
 <template>
-  <div id="app">
-    <p>Welcome to SIDCloud</p>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-app-bar>
 
-    <audio id="radio" controls preload="none">
-      <source src="http://localhost:8099/api/v1/audio" type="audio/wav" />
-    </audio>
-    <p />
-
-    <input
-      class="form-control"
-      v-model="sid_link"
-      v-on:keyup.enter="Link"
-      placeholder="Type SID or PRG file link"
-    />
-    <p>{{ sid_data }}</p>
-
-    <input
-      class="form-control"
-      v-model="handle_id"
-      v-on:keyup.enter="Handle"
-      placeholder="Type scener handle"
-    />
-    <p>{{ handle_name }}</p>
-  </div>
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import axios from "axios";
+import HelloWorld from './components/HelloWorld';
 
 export default {
-  data: function() {
-    return {
-      handle_id: 0,
-      handle_name: null,
-      info: null,
-      sid_link: null,
-      sid_data: null,
-      query_url: ""
-    };
+  name: 'App',
+  components: {
+    HelloWorld,
   },
-  name: "app",
-  components: {},
-  methods: {
-    // ==========================================================
-    // Odczyt danych o scenerze
-    // ==========================================================
-    Handle: function() {
-      var query =
-        "https://csdb.dk/webservice/?type=scener&id=" + this.handle_id;
-      // eslint-disable-next-line
-      console.log("Query = " + query);
-
-      axios.get(query).then(response => (this.handle_name = response.data));
-
-      // eslint-disable-next-line
-      console.log("Scener " + this.handle_name);
-    },
-
-    // ==========================================================
-    // Odtworzenie SIDa
-    // ==========================================================
-    Link: function() {
-      var query = "http://localhost:8099/api/v1/audio?sid_url=" + this.sid_link;
-      // eslint-disable-next-line
-      console.log("Query = " + query);
-
-      var player = document.getElementById("radio");
-
-      player.pause();
-      player.currentTime = 0.0;
-
-      axios.post(query).then(response => {
-        this.sid_data = response.data;
-
-        // eslint-disable-next-line
-        console.log("SID data " + this.sid_data);
-
-        player.load();
-        player.play();
-      });
-
-      // player.currentTime = 0.0;
-      // player.pause();
-      // player.currentTime = 0.0;
-    }
-  }
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  /* -moz-osx-font-smoothing: grayscale; */
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import vuetify from './plugins/vuetify';
 
 axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
 
@@ -8,5 +9,6 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  vuetify,
   axios
 }).$mount('#app')
