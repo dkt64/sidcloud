3f10c7ffe471d248f3e36694d0d82192ef7a3ae8
dkt64 <b.apanasewicz@gmail.com>
2019-11-12 00:35:27 +0100

    csdb service v1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
+
 <template>
-  <div id="app" @drop.prevent="addFile" @dragover.prevent @dragover.capture="dragingOn" @drop.capture="dragingOff"> 
-    <h3 class="display-4">Welcome to SIDCloud</h3>
+  <!--
+  =================================================================================================
+  TEMPLATE
+  =================================================================================================
+  -->
+  <div
+    id="app"
+    @drop.prevent="addFile"
+    @dragover.prevent
+    @dragover.capture="dragingOn"
+    @drop.capture="dragingOff"
+  >
+    <h1>Welcome to SIDCloud</h1>
 
     <audio id="radio" controls preload="none" loop>
       <source src="http://sidcloud.net/api/v1/audio" type="audio/wav" />
@@ -21,57 +33,90 @@
       <div class="input-group-append" id="button-addon4">
         <button
           type="button"
-          style="margin-right: 20px; margin-left: 5px"
+          style="margin-right: 10px; margin-left: 10px"
           class="btn btn-success"
           :disabled="dragin"
           v-on:click="Link"
-        >Stream Go...</button>
+        >Stream SID...</button>
+      </div>
+      <div class="input-group-append" id="button-addon4">
+        <button
+          type="button"
+          style="margin-right: 20px"
+          class="btn btn-success"
+          :disabled="dragin"
+          v-on:click="Next"
+        >Next release</button>
       </div>
     </div>
 
     <p />
+    <p>{{ csdb_release_name }} {{ csdb_release_group }}</p>
+    <!-- <p>Music by {{ csdb_release_credits }}</p> -->
 
-    <div class="alert alert-light" role="alert">{{ sid_data }}</div>
+    <!-- <p>Response from sidcloud server: {{ response_from_server }}</p> -->
 
-    <!-- <input
-      class="form-control"
-      v-model="handle_id"
-      v-on:keyup.enter="Handle"
-      placeholder="Type scener handle"
-    />
-    <p>{{ handle_name }}</p>-->
+    <!-- <p>CSDB release ID: {{ csdb_release_id }}</p>
+    <p>CSDB release download links: {{ csdb_download_links }}</p>
+    <p>CSDB release: {{ csdb_release }}</p>
+    <p>CSDB release data: {{ csdb_release_data }}</p>-->
   </div>
 </template>
 
 <script>
+// ================================================================================================
+// SCRIPT
+// ================================================================================================
+
 import axios from "axios";
 
 export default {
   data: function() {
     return {
       handle_id: 0,
-      handle_name: null,
+      csdb_releases: null,
+      csdb_release: null,
+      csdb_release_id: null,
+      csdb_release_data: null,
+      csdb_download_links: null,
+      csdb_release_name: "",
+      csdb_release_group: "",
+      csdb_release_credits: "",
+      release_nr: 1,
       info: null,
       sid_link: null,
-      sid_data: null,
+      response_from_server: null,
       query_url: "",
       file: null,
-      dragin: false,
+      dragin: false
     };
   },
   name: "app",
   components: {},
+
+  // ==============================================================================================
+  // METODY VUE
+  // ==============================================================================================
+
+  created: function() {
+    this.GetCSDBData();
+  },
+
+  // ==============================================================================================
+  // METODY
+  // ==============================================================================================
+
   methods: {
     // ==========================================================
     // Draging
     // ==========================================================
     dragingOn() {
-      // this.sid_data = "dragin..";
-      this.dragin = true
+      // this.response_from_server = "dragin..";
+      this.dragin = true;
     },
     dragingOff() {
-      // this.sid_data = "dragin..";
-      this.dragin = false
+      // this.response_from_server = "dragin..";
+      this.dragin = false;
     },
     // ==========================================================
     // Drag & Drop
@@ -95,25 +140,192 @@ export default {
         // eslint-disable-next-line
         console.log("File sent. Response: ", response.data);
 
-        this.sid_data = response.data;
+        this.response_from_server = response.data;
 
+        player.src = "";
+        player.src = "http://sidcloud.net/api/v1/audio";
         player.load();
         player.play();
       });
     },
     // ==========================================================
-    // Odczyt danych o scenerze
+    // Następna produkcja
     // ==========================================================
-    Handle: function() {
-      var query =
-        "https://csdb.dk/webservice/?type=scener&id=" + this.handle_id;
-      // eslint-disable-next-line
-      console.log("Query = " + query);
+    Next: function() {
+      this.GetCSDBData();
+    },
+    // ==========================================================
+    // Odczyt danych z CSDB
+    // ==========================================================
+    GetCSDBData: function() {
+      var query;
 
-      axios.get(query).then(response => (this.handle_name = response.data));
+      // Latest releases
+      // --------------------------------------------------------
+      query = "https://csdb.dk/rss/latestreleases.php";
 
-      // eslint-disable-next-line
-      console.log("Scener " + this.handle_name);
+      axios.get(query).then(response => {
+        this.csdb_releases = response.data;
+
+        var oParser = new DOMParser();
+
+        // Pętla w której szukamy typu release - przerywamy fora gdy znajdziemy
+        // --------------------------------------------------------------------
+        var i;
+        for (i = 0; i < 100; i++) {
+          this.csdb_release = oParser
+            .parseFromString(this.csdb_releases, "application/xml")
+            .getElementsByTagName("description")[
+            this.release_nr++
+          ].childNodes[0].nodeValue;
+
+          if (
+            this.csdb_release.indexOf(">C64 Demo</a><br /><a href=") > 0 ||
+            this.csdb_release.indexOf(">C64 One-File Demo</a><br /><a href=") >
+              0 ||
+            this.csdb_release.indexOf(">C64 Intro</a><br /><a href=") > 0 ||
+            this.csdb_release.indexOf(">C64 4K Intro</a><br /><a href=") > 0 ||
+            this.csdb_release.indexOf(">C64 Crack intro</a><br /><a href=") >
+              0 ||
+            this.csdb_release.indexOf(">C64 REU Release</a><br /><a href=") >
+              0 ||
+            this.csdb_release.indexOf(">C64 Music</a><br /><a href=") > 0 ||
+            this.csdb_release.indexOf(
+              ">C64 Music Collection</a><br /><a href="
+            ) > 0 ||
+            this.csdb_release.indexOf(
+              ">C64 Graphics Collection</a><br /><a href="
+            ) > 0 ||
+            this.csdb_release.indexOf(">C64 Diskmag</a><br /><a href=") > 0 ||
+            this.csdb_release.indexOf(">C64 Charts</a><br /><a href=") > 0 ||
+            this.csdb_release.indexOf(">C64 Invitation</a><br /><a href=") >
+              0 ||
+            this.csdb_release.indexOf(">C64 1K Intro</a><br /><a href=") > 0 ||
+            this.csdb_release.indexOf(">C64 Fake Demo</a><br /><a href=") > 0 ||
+            this.csdb_release.indexOf(">C128 Release</a><br /><a href=") > 0 ||
+            this.csdb_release.indexOf(">SuperCPU Release</a><br /><a href=") >
+              0 ||
+            this.csdb_release.indexOf(">C64 DTV</a><br /><a href=") > 0 ||
+            this.csdb_release.indexOf(">C64 Fake Demo</a><br /><a href=") > 0
+          ) {
+            break;
+          }
+        }
+
+        // Wyciągamy numer ID tej produkcji
+        // -------------------------------------------------------------------
+        this.csdb_release_id = oParser
+          .parseFromString(this.csdb_releases, "application/xml")
+          .getElementsByTagName("link")[
+          this.release_nr
+        ].childNodes[0].nodeValue;
+
+        this.csdb_release_id = parseInt(
+          this.csdb_release_id.replace(/^[^0-9]+/, ""),
+          10
+        );
+
+        // Odczytujemy dane release i szukamy SID'a a potem PRG
+        // -------------------------------------------------------------------
+
+        query =
+          "https://csdb.dk/webservice/?type=release&id=" +
+          this.csdb_release_id +
+          "&depth=2";
+
+        axios.get(query).then(response => {
+          this.csdb_release_data = response.data;
+
+          // var oParser = new DOMParser();
+          // this.csdb_download_links = oParser
+          //   .parseFromString(this.csdb_release_data, "application/xml")
+          //   .getElementsByTagName("DownloadLinks")[0].childNodes[0].textContent;
+
+          var links_count = (this.csdb_release_data.match(/<Link>/g) || [])
+            .length;
+          // eslint-disable-next-line
+          console.log("Liczba linków: " + links_count);
+
+          this.csdb_download_links = links_count;
+
+          var i;
+          var link;
+          var sid_found = false;
+
+          this.sid_link = "";
+
+          // szukamy SID
+          for (i = 0; i < links_count; i++) {
+            link = oParser
+              .parseFromString(this.csdb_release_data, "application/xml")
+              .getElementsByTagName("Link")[i].childNodes[0].nodeValue;
+
+            if (link.indexOf(".sid") > 0) {
+              sid_found = true;
+              break;
+            }
+          }
+
+          if (!sid_found) {
+            // szukamy PRG
+            for (i = 0; i < links_count; i++) {
+              link = oParser
+                .parseFromString(this.csdb_release_data, "application/xml")
+                .getElementsByTagName("Link")[i].childNodes[0].nodeValue;
+
+              if (link.indexOf(".prg") > 0) {
+                break;
+              }
+            }
+          }
+
+          if (sid_found) {
+            // Wpisujemy LINK do kontrolki na stronie
+            this.sid_link = link;
+
+            // Nazwa produkcji
+            this.csdb_release_name = oParser
+              .parseFromString(this.csdb_release_data, "application/xml")
+              .getElementsByTagName("Name")[0].childNodes[0].nodeValue;
+
+            // Grupy
+            var groups_count = (this.csdb_release_data.match(/<Group>/g) || [])
+              .length;
+            // eslint-disable-next-line
+            console.log("Liczba grup: " + groups_count);
+
+            if (groups_count > 0) {
+              this.csdb_release_group = "by ";
+            } else {
+              this.csdb_release_group = "";
+            }
+            var przecinek = " ";
+            for (i = 2; i < groups_count + 2; i++) {
+              if (i > 2) {
+                przecinek = ", ";
+              }
+              this.csdb_release_group +=
+                przecinek +
+                oParser
+                  .parseFromString(this.csdb_release_data, "application/xml")
+                  .getElementsByTagName("Name")[i].childNodes[0].nodeValue;
+            }
+
+            // Credits
+            var credits_count = (
+              this.csdb_release_data.match(/<Credit>/g) || []
+            ).length;
+            // eslint-disable-next-line
+            console.log("Liczba credits: " + groups_count);
+
+            // this.csdb_release_credits = oParser
+            //   .parseFromString(this.csdb_release_data, "application/xml")
+            //   .getElementsByTagName("Handle")[0].childNodes[0].nodeValue;
+          } else {
+            this.GetCSDBData();
+          }
+        });
+      });
     },
 
     // ==========================================================
@@ -130,10 +342,10 @@ export default {
       player.currentTime = 0.0;
 
       axios.post(query).then(response => {
-        this.sid_data = response.data;
+        this.response_from_server = response.data;
 
         // eslint-disable-next-line
-        console.log("SID data " + this.sid_data);
+        console.log("SID data " + this.response_from_server);
 
         player.load();
         player.play();
@@ -144,6 +356,11 @@ export default {
 </script>
 
 <style>
+/* 
+===================================================================================================
+STYLE
+===================================================================================================
+*/
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
